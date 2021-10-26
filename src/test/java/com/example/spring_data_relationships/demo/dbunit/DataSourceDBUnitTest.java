d86827173a894084702fa136ad363a4ef7185e15
anton.palshyn <palshyn@me.com>
2021-10-26 18:36:30 +0300

    DbUnit. Service layer testing

--- src/test/java/com/example/spring_data_relationships/demo/dbunit/DataSourceDBUnitTest.java
+++ src/test/java/com/example/spring_data_relationships/demo/dbunit/DataSourceDBUnitTest.java
@@ -126,10 +126,6 @@ public class DataSourceDBUnitTest extends DataSourceBasedDBTestCase {
         }
     }
 
-
-
-
-
     @Test
     public void departmentTableGivenDataSet_whenSelect_thenFirstNameIsName7() throws SQLException {
         ResultSet rs = connection.createStatement().executeQuery("select * from department_table where id = 7");
@@ -173,4 +169,60 @@ public class DataSourceDBUnitTest extends DataSourceBasedDBTestCase {
             Assertion.assertEquals(expectedTable, actualTable);
         }
     }
+
+    @Test
+    public void testGetById() throws Exception {
+
+    }
+
+//
+//    @ContextConfiguration(locations = "classpath:application-context-test.xml")
+//    @RunWith(SpringJUnit4ClassRunner.class)
+//    public class TestEmployeeDAO
+//    {
+//
+//        @Autowired
+//        private EmployeeDAO employeeDAO;
+//
+//        @Autowired
+//        private DepartmentDAO departmentDAO;
+//
+//        @Test
+//        @Transactional
+//        @Rollback(true)
+//        public void testAddDepartment()
+//        {
+//            DepartmentEntity department = new DepartmentEntity("Information Technology");
+//            departmentDAO.addDepartment(department);
+//
+//            List<DepartmentEntity> departments = departmentDAO.getAllDepartments();
+//            Assert.assertEquals(department.getName(), departments.get(0).getName());
+//        }
+//
+//        @Test
+//        @Transactional
+//        @Rollback(true)
+//        public void testAddEmployee()
+//        {
+//            DepartmentEntity department = new DepartmentEntity("Human Resource");
+//            departmentDAO.addDepartment(department);
+//
+//            EmployeeEntity employee = new EmployeeEntity();
+//            employee.setFirstName("Lokesh");
+//            employee.setLastName("Gupta");
+//            employee.setEmail("howtodoinjava@gmail.com");
+//            employee.setDepartment(department);
+//
+//            employeeDAO.addEmployee(employee);
+//
+//            List<DepartmentEntity> departments = departmentDAO.getAllDepartments();
+//            List<EmployeeEntity> employees = employeeDAO.getAllEmployees();
+//
+//            Assert.assertEquals(1, departments.size());
+//            Assert.assertEquals(1, employees.size());
+//
+//            Assert.assertEquals(department.getName(), departments.get(0).getName());
+//            Assert.assertEquals(employee.getEmail(), employees.get(0).getEmail());
+//        }
+//    }
 }
